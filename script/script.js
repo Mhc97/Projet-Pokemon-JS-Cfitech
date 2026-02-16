779ad335f60f8dc3e8acb58438f76e6bc45ddf87
Mouhcine <mouhcine@gmail.com>
2026-02-16 12:23:07 +0100

    feat: initial project setup with pokemon data

---
+++ script/script.js
@@ -0,0 +1,34 @@
+let pkmList = [
+// Dracaufeu
+
+   ['Dracaufeu', './assets/image/800px-Dracaufeu-RFVF.png', 360, [
+ ['Lance-flammes', 'feu', 95, 0.90],
+ ['Vol', 'vol', 90, 0.95],
+ ['Danse-flamme', 'feu', 85, 0.88],
+ ['Griffe-acier', 'acier', 75, 0.92]
+ ]],
+
+//  Tortank
+
+ [
+    ['Tortank', './assets/image/800px-Méga-Tortank-XY.png', 350,[
+        ['Surf', 'eau', 90, 0.95],
+        ['Hydrocanon', 'eau', 110, 0.80],
+        ['Morsure', 'eau', 75, 0.95],
+        ['Protection', 'normal', 0, 1.00]
+    ]
+ ]],
+
+// Racaouille
+[
+    ['Racaillou', 'https://www.pokepedia.fr', 290, [
+        ['Jet-Pierres', 'roche', 50, 0.90],
+        ['Séisme', 'sol', 100, 1.00],
+        ['Roulade', 'roche', 30, 0.90],
+        ['Boul\'Armure', 'normal', 0, 1.00]
+    ]]
+]
+
+
+
+]
